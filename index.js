9bec7f157be4193cf5c1a0f89d17180b85eea0f9
Nick Paolino <github email address>
2017-10-30 17:46:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.map(function(driver){
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers, hometown){
+  const newDrivers = drivers.filter(function(driver){
+    return driver.hometown === hometown
+  })
+  newDrivers.map(function(driver){
+    console.log(driver.name)
+  })
+}
+
+const driversByRevenue = function (drivers){
+  return drivers.slice().sort(function(a, b){
+    return a.revenue - b.revenue
+  })
+}
+
+const driversByName = function (drivers){
+  return (drivers.slice().sort(function(first, second){
+    return first.name.localeCompare(second.name)
+  }))
+}
+
+const totalRevenue = function(drivers){
+  return drivers.reduce(function(total, currentDriver){
+    return currentDriver.revenue + total
+  }, 0)
+}
+
+const averageRevenue = function(drivers){
+  return totalRevenue(drivers) / drivers.length
+}
